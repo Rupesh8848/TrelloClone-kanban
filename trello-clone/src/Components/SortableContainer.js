39b394cd5e9079a4b33fa4b7805ee64038109a43
Rupesh8848 <shrestharupesh02@gmail.com>
2023-07-06 13:40:28 +0545

    Container can be dragged as well

--- trello-clone/src/Components/SortableContainer.js
+++ trello-clone/src/Components/SortableContainer.js
@@ -1,29 +1,46 @@
-import { useDroppable } from "@dnd-kit/core";
+import { useDraggable, useDroppable } from "@dnd-kit/core";
 import {
   SortableContext,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import React from "react";
 import SortableItem from "./SortableItem";
+import { CSS } from "@dnd-kit/utilities";
 
 export default function SortableContainer({ data, title }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef: containerRef,
+    transform,
+    transition,
+    isDragging,
+  } = useDraggable({ id: title });
+
   const { setNodeRef } = useDroppable({ id: title });
 
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    opacity: isDragging ? "0.6" : "1",
+  };
   return (
-    <SortableContext
-      id={title}
-      items={data}
-      strategy={verticalListSortingStrategy}
-    >
-      <div
-        className="bg-[#F1F2F4] flex flex-col justify-center rounded-xl overflow-hidden w-[25rem] px-[7px] h-[100%] mr-4"
-        ref={setNodeRef}
+    <div ref={containerRef} style={style} {...attributes} {...listeners}>
+      <SortableContext
+        id={title}
+        items={data}
+        strategy={verticalListSortingStrategy}
       >
-        <h3 className="my-[1rem] ml-4 text-[#2C3E5D]">{title}</h3>
-        {data.map((datum) => (
-          <SortableItem key={datum.id} datum={datum} />
-        ))}
-      </div>
-    </SortableContext>
+        <div
+          className="bg-[#F1F2F4] flex flex-col justify-center rounded-xl overflow-hidden w-[25rem] px-[7px]  mr-4"
+          ref={setNodeRef}
+        >
+          <h3 className="my-[1rem] ml-4 text-[#2C3E5D]">{title}</h3>
+          {data.map((datum) => (
+            <SortableItem key={datum.id} datum={datum} />
+          ))}
+        </div>
+      </SortableContext>
+    </div>
   );
 }
--- trello-clone/src/routes/MainContainer.js
+++ trello-clone/src/routes/MainContainer.js
@@ -5,12 +5,14 @@ import {
   PointerSensor,
   closestCorners,
   defaultDropAnimation,
+  useDroppable,
   useSensor,
   useSensors,
 } from "@dnd-kit/core";
 import {
   SortableContext,
   arrayMove,
+  horizontalListSortingStrategy,
   sortableKeyboardCoordinates,
 } from "@dnd-kit/sortable";
 import React, { useState } from "react";
@@ -27,7 +29,13 @@ export default function MainContainer() {
   const [boardSections, setBoardSections] =
     React.useState(initialBoardSections);
 
+  const [containers, setContainers] = React.useState(
+    Object.keys(boardSections)
+  );
+
   const [activeTask, setActiveTask] = useState(null);
+
+  const [activeTContainer, setActiveTContainer] = useState(null);
 
   const sensors = useSensors(
     useSensor(PointerSensor),
@@ -37,81 +45,109 @@ export default function MainContainer() {
   //handler functions
 
   const handleDragStart = (event) => {
-    setActiveTask(event.active.id); //active item is an object of current item
+    if (containers.indexOf(event.active.id) !== -1) {
+      setActiveTContainer(event.active.id);
+    } else {
+      setActiveTask(event.active.id); //active item is an object of current item
+    }
   };
 
   const handleDragOver = (event) => {
     const { active, over } = event;
 
-    const activeContainer = findBoardSectionContainer(boardSections, active.id);
-
-    const overContainer = findBoardSectionContainer(boardSections, over.id);
-
-    if (
-      !activeContainer ||
-      !overContainer ||
-      activeContainer === overContainer
-    ) {
+    if (activeTContainer) {
       return;
-    }
-
-    setBoardSections((prevState) => {
-      const activeItems = prevState[activeContainer];
-      const overItems = prevState[overContainer];
-
-      const activeIndex = activeItems.findIndex(
-        (item) => item.id === active.id
+    } else {
+      const activeContainer = findBoardSectionContainer(
+        boardSections,
+        active.id
       );
 
-      const overIndex = overItems.findIndex((item) => item.id !== over?.id);
-      return {
-        ...prevState,
-        [activeContainer]: [
-          ...prevState[activeContainer].filter((item) => item.id !== active.id),
-        ],
-        [overContainer]: [
-          ...prevState[overContainer].slice(0, overIndex),
-          prevState[activeContainer][activeIndex],
-          ...prevState[overContainer].slice(
-            overIndex,
-            prevState[overContainer].length
-          ),
-        ],
-      };
-    });
+      const overContainer = findBoardSectionContainer(boardSections, over.id);
+
+      if (
+        !activeContainer ||
+        !overContainer ||
+        activeContainer === overContainer
+      ) {
+        return;
+      }
+
+      setBoardSections((prevState) => {
+        const activeItems = prevState[activeContainer];
+        const overItems = prevState[overContainer];
+
+        const activeIndex = activeItems.findIndex(
+          (item) => item.id === active.id
+        );
+
+        const overIndex = overItems.findIndex((item) => item.id !== over?.id);
+        return {
+          ...prevState,
+          [activeContainer]: [
+            ...prevState[activeContainer].filter(
+              (item) => item.id !== active.id
+            ),
+          ],
+          [overContainer]: [
+            ...prevState[overContainer].slice(0, overIndex),
+            prevState[activeContainer][activeIndex],
+            ...prevState[overContainer].slice(
+              overIndex,
+              prevState[overContainer].length
+            ),
+          ],
+        };
+      });
+    }
   };
 
   const handleDragEnd = ({ active, over }) => {
-    const activeContainer = findBoardSectionContainer(boardSections, active.id);
+    if (activeTContainer) {
+      if (active.id !== over.id) {
+        setContainers((oldState) => {
+          const oldIndex = oldState.indexOf(active.id);
+          const newIndex = oldState.indexOf(over.id);
+          const newArray = arrayMove(oldState, oldIndex, newIndex);
+          return newArray;
+        });
+      }
+      setActiveTContainer(null);
+    } else {
+      const activeContainer = findBoardSectionContainer(
+        boardSections,
+        active.id
+      );
 
-    const overContainer = findBoardSectionContainer(boardSections, over?.id);
+      const overContainer = findBoardSectionContainer(boardSections, over?.id);
 
-    if (
-      !activeContainer ||
-      !overContainer ||
-      activeContainer !== overContainer
-    ) {
-      return;
+      if (
+        !activeContainer ||
+        !overContainer ||
+        activeContainer !== overContainer
+      ) {
+        return;
+      }
+
+      const activeIndex = boardSections[activeContainer].findIndex(
+        (item) => item.id === active.id
+      );
+
+      const overIndex = boardSections[overContainer].findIndex(
+        (item) => item.id === over.id
+      );
+      if (activeIndex !== overIndex) {
+        setBoardSections((prevState) => ({
+          ...prevState,
+          [overContainer]: arrayMove(
+            prevState[overContainer],
+            activeIndex,
+            overIndex
+          ),
+        }));
+      }
+      setActiveTask(null);
     }
-
-    const activeIndex = boardSections[activeContainer].findIndex(
-      (item) => item.id === active.id
-    );
-
-    const overIndex = boardSections[overContainer].findIndex(
-      (item) => item.id === over.id
-    );
-    if (activeIndex !== overIndex) {
-      setBoardSections((prevState) => ({
-        ...prevState,
-        [overContainer]: arrayMove(
-          prevState[overContainer],
-          activeIndex,
-          overIndex
-        ),
-      }));
-    }
-    setActiveTask(null);
   };
 
   const dropAnimation = {
@@ -119,6 +155,8 @@ export default function MainContainer() {
   };
 
   const currentTask = getTaskById(tasks, activeTask);
+
+  const { setNodeRef } = useDroppable({ id: "root" });
 
   return (
     <div className="h-[100vh] min-w-[100vw]   bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex  items-center px-[1rem] overflow-x-scroll">
@@ -129,18 +167,29 @@ export default function MainContainer() {
         onDragEnd={handleDragEnd}
         onDragOver={handleDragOver}
       >
-        <div className="flex justify-evenly">
-          {Object.keys(boardSections).map((boardSectionKey) => (
-            <SortableContainer
-              key={boardSectionKey}
-              data={boardSections[boardSectionKey]}
-              title={boardSectionKey}
-            />
-          ))}
-        </div>
+        <SortableContext
+          items={containers}
+          strategy={horizontalListSortingStrategy}
+        >
+          <div ref={setNodeRef} className="flex justify-evenly">
+            {containers.map((boardSectionKey) => (
+              <SortableContainer
+                key={boardSectionKey}
+                data={boardSections[boardSectionKey]}
+                title={boardSectionKey}
+              />
+            ))}
+          </div>
+        </SortableContext>
 
         <DragOverlay dropAnimation={dropAnimation}>
           {activeTask ? <SortableItem datum={currentTask} /> : null}
+          {activeTContainer ? (
+            <SortableContainer
+              data={boardSections[activeTContainer]}
+              title={activeTContainer}
+            />
+          ) : null}
         </DragOverlay>
       </DndContext>
     </div>
